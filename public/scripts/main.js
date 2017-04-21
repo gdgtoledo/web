66a1ca947ad5e7e9a1493aff3a42f16b11755f5b
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-04-21 08:27:13 +0200

    Load index data from data container

---
+++ public/scripts/main.js
@@ -0,0 +1,33 @@
+function getProfiles() {
+    return WeDeploy.data('data.b612.wedeploy.me')
+        .get('profiles')
+        .then(function(profiles) {
+            plotProfiles(profiles);
+
+		    return this;
+		});
+}
+
+function plotProfiles(profiles) {
+    var list = document.getElementById('members');
+
+    var elements = '';
+
+    for(var i = 0; i < profiles.length; i++) {
+        elements += plotProfile(profiles[i]);
+    }
+
+    list.innerHTML = elements;
+}
+
+function plotProfile(profile) {
+    var memberElement = '';
+
+    memberElement += `<li class="member">
+    <a>
+        <img src="${member.pictureUrl}" alt="${member.commonName}" />
+    </a>
+</li>`;
+
+    return memberElement;
+}
