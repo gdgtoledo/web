1b963382d0ae134718ca87c56aff4845f59972a7
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-04-24 09:04:13 +0200

    covert to modules inside craft object

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-function getProfiles() {
+/*function getProfiles() {
     return WeDeploy.data('data.b612.wedeploy.me')
         .get('profiles')
         .then(function(profiles) {
@@ -33,4 +33,66 @@ function plotProfile(profile) {
 </li>`;
 
     return profileElement;
-}
+}*/
+
+
+var craft = caft || {};
+
+craft.member = (function () {
+
+    function render(profile) {
+        var member = member || {};
+
+        member.rendered = '';
+        member.information = profile.personal_information;
+
+        member.rendered += `
+            <li class="member">
+                <a>
+                    <img src="${member.information.picture_url}" 
+                        title="${member.name} ${member.surname}" 
+                        alt="${member.name} ${member.surname}" />
+                </a>
+            </li>`;
+
+        return member.rendered;
+
+    }
+
+    return {
+        render: render,
+    }
+
+})();
+
+craft.members = (function () {
+
+    function render(profiles) {
+        var members = members || {};
+        
+        members.rendered = '',
+        members.node = document.getElementById('members');
+        
+        for(var p = 0; p < profiles.length; p++) {
+            members.rendered += carft.member.render(profiles[p]);
+        }
+
+        members.node.innerHTML = members.rendered;
+
+    }
+
+    function get() {
+        return WeDeploy.data('data.b612.wedeploy.me')
+            .get('profiles')
+            .then(function(profiles) {
+                craft.members.render(profiles);
+        });
+    }
+
+    return {
+        get: get,
+    }
+
+})();
+
+craft.members.get();
